e91118a1a78ea14a39cc5fe6c4d5bb4545c89ea7
1234sssssdddd <34847574+1234sssssdddd@users.noreply.github.com>
2019-02-15 22:20:39 +0900

    QuestionBank 생성 (미완성)
    GameViewController에서 문제바뀔 때 업데이트되는 값 수정했음

---
+++ memoji-master/memoji/CategoryTableViewController.swift
@@ -0,0 +1,154 @@
+//
+//  CategoryTableViewController.swift
+//  memoji
+//
+//  Created by CAU on 07/02/2019.
+//  Copyright © 2019 purplxider. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+    
+    let categories = ["KPOP", "드라마", "영화"] // 수정
+    var money = UserDefaults.standard.integer(forKey: "money")
+    let moneyButton = UIButton(type: .system)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+        setupNavigationBar()
+        setupMoneyButton()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        setupToolbar()
+        
+        money = UserDefaults.standard.integer(forKey: "money")
+        moneyButton.setTitle(" \(money)", for: .normal)
+        moneyButton.sizeToFit()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        
+        navigationController?.setToolbarHidden(true, animated: false)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return categories.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+
+        // Configure the cell...
+        
+        let categoryName = categories[indexPath.row]
+        cell.textLabel?.text = categoryName
+        cell.backgroundColor = .clear
+        
+        return cell
+    }
+    
+    func setupTableView() {
+        self.tableView.rowHeight = 80.0
+        
+        tableView.backgroundView = UIImageView(image: UIImage(named: "background.png"))
+        tableView.backgroundColor = .clear
+    }
+    
+    func setupToolbar() {
+        navigationController?.setToolbarHidden(false, animated: true)
+        navigationController?.toolbar.setBackgroundImage(UIImage(), forToolbarPosition: .any, barMetrics: .default)
+        navigationController?.toolbar.clipsToBounds = true
+        navigationController?.toolbar.tintColor = UIColor(red: 228/255.0, green: 175/255.0, blue: 10/255.0, alpha: 1.0)
+        let plus = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil) // 수정
+        let favorite = UIBarButtonItem(image: UIImage(named: "filledHeart.png"), style: .plain, target: self, action: nil) // 수정
+        let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        var items = [UIBarButtonItem]()
+        items.append(plus)
+        items.append(space)
+        items.append(favorite)
+        self.toolbarItems = items
+    }
+    
+    func setupMoneyButton() {
+        moneyButton.setImage(UIImage(named: "user.png"), for: .normal)
+        moneyButton.setTitle(" \(money)", for: .normal)
+        moneyButton.titleLabel?.font = UIFont.systemFont(ofSize: 22.0, weight: .semibold)
+        moneyButton.sizeToFit()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: moneyButton)
+    }
+    
+    func setupNavigationBar() {
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.clipsToBounds = true
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ memoji-master/memoji/QuestionBank.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionBank.swift
+//  memoji
+//
+//  Created by CAU on 15/02/2019.
+//  Copyright © 2019 CAU. All rights reserved.
+//
+
+import Foundation
+
+class QuestionBank {
+    var list = [Question] ()
+    
+    init() {
+        list.append(Question(emoji: "🐮🙏✋🙋‍♀️❤👉🙎‍♂️", length :3, answer:["거", "짓", "말"], answerPool:["따", "르", "릉", "모", "여", "라", "바", "코", "드"]))
+//        list.append(Question(emoji: "🙏🙋‍♂️👉🏃‍♀️", length :2, answer:["고", "백"], answerPool:["바", "다", "박", "수", "싸", "이", "바", "코", "드"]))
+//        list.append(Question(emoji: "🙋‍♂️❤️👩💨", length :5, answer:["그", "대", "가", "분", "댜"], answerPool:["바", "다", "박", "수", "싸", "이"]))
+        
+    }
+}
+
